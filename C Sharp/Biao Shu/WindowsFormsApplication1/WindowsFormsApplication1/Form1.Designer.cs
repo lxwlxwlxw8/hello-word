9ba2d5c7294c814ed7b99f0b9fa0f19efad8d7d6
RobertX <370529651@qq.com>
2018-04-14 16:08:50 +0800

    Add files via upload

---
+++ C Sharp/Biao Shu/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_fileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_totalNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.differGridView = new System.Windows.Forms.DataGridView();
+            this.rowNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.excelNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.phName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rowNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.differGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(474, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_fileName
+            // 
+            this.tb_fileName.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_fileName.Location = new System.Drawing.Point(77, 18);
+            this.tb_fileName.Multiline = true;
+            this.tb_fileName.Name = "tb_fileName";
+            this.tb_fileName.ReadOnly = true;
+            this.tb_fileName.Size = new System.Drawing.Size(391, 23);
+            this.tb_fileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "文件位置：";
+            // 
+            // textBox_totalNum
+            // 
+            this.textBox_totalNum.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_totalNum.Location = new System.Drawing.Point(77, 61);
+            this.textBox_totalNum.Name = "textBox_totalNum";
+            this.textBox_totalNum.ReadOnly = true;
+            this.textBox_totalNum.Size = new System.Drawing.Size(70, 26);
+            this.textBox_totalNum.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "总数量：";
+            // 
+            // differGridView
+            // 
+            this.differGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.differGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rowNo,
+            this.excelNum,
+            this.countNum});
+            this.differGridView.Location = new System.Drawing.Point(347, 60);
+            this.differGridView.Name = "differGridView";
+            this.differGridView.RowTemplate.Height = 23;
+            this.differGridView.Size = new System.Drawing.Size(292, 93);
+            this.differGridView.TabIndex = 5;
+            // 
+            // rowNo
+            // 
+            this.rowNo.HeaderText = "行号";
+            this.rowNo.Name = "rowNo";
+            this.rowNo.ReadOnly = true;
+            this.rowNo.Width = 60;
+            // 
+            // excelNum
+            // 
+            this.excelNum.HeaderText = "excel数量";
+            this.excelNum.Name = "excelNum";
+            this.excelNum.ReadOnly = true;
+            this.excelNum.Width = 90;
+            // 
+            // countNum
+            // 
+            this.countNum.HeaderText = "计算数量";
+            this.countNum.Name = "countNum";
+            this.countNum.ReadOnly = true;
+            this.countNum.Width = 80;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "个";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(229, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 14);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "每行数量差异表:";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.phName,
+            this.rowNum});
+            this.dataGridView2.Location = new System.Drawing.Point(10, 178);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(629, 241);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // phName
+            // 
+            this.phName.HeaderText = "编号名称";
+            this.phName.Name = "phName";
+            this.phName.ReadOnly = true;
+            this.phName.Width = 150;
+            // 
+            // rowNum
+            // 
+            this.rowNum.HeaderText = "行号";
+            this.rowNum.Name = "rowNum";
+            this.rowNum.ReadOnly = true;
+            this.rowNum.Width = 350;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(12, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 14);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "重复编号表:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(556, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "分析数据";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 430);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.differGridView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_totalNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_fileName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "出厂编号识别工具";
+            ((System.ComponentModel.ISupportInitialize)(this.differGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_fileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_totalNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView differGridView;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rowNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn excelNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rowNum;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ C Sharp/Biao Shu/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using NPOI.SS.UserModel;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "所有文件(*xls*)|*.xls*"; //设置要选择的文件的类型
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = fileDialog.FileName;//返回文件的完整路径 
+                tb_fileName.Text = path;
+                doAnalysisDate(path);
+            }
+        }
+
+        /// <summary>
+        /// 分析excel数据
+        /// </summary>
+        /// <param name="path">文件路径</param>
+        private void doAnalysisDate(String path)
+        {
+            differGridView.Rows.Clear();
+            dataGridView2.Rows.Clear();
+
+            tb_fileName.Text = path;
+
+            IWorkbook workbook = null;  //新建IWorkbook对象
+            FileStream fileStream = null;
+            try
+            {
+
+                fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                if (path.IndexOf(".xlsx") > 0) // 2007版本  
+                {
+                    workbook = new XSSFWorkbook(fileStream);  //xlsx数据读入workbook  
+                }
+                else if (path.IndexOf(".xls") > 0) // 2003版本  
+                {
+                    workbook = new HSSFWorkbook(fileStream);  //xls数据读入workbook  
+                }
+                differGridView.Rows.Clear();
+                dataGridView2.Rows.Clear();
+
+                ISheet sheet = workbook.GetSheetAt(0);  //获取第一个工作表  
+                //进行数据过滤
+                List<String[]> allItems = getAllItemsList(sheet);
+                textBox_totalNum.Text = allItems.Count.ToString();
+                doQuery(allItems);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message, "异常");
+            }
+            finally
+            {
+                if (fileStream != null) {
+                    fileStream.Close();
+                }
+                if(workbook != null)
+                {
+                    workbook.Close();
+                }
+                
+            }
+        }
+
+        private List<String[]> getAllItemsList(ISheet sheet)
+        {
+            //所有牌号对象
+            List<String[]> allItems = new List<String[]>();
+            //记录牌号数量存异的信息
+            //Object[] everyRowNum = new Object[sheet.LastRowNum-3];  //
+
+            IRow row;
+            Console.WriteLine("每行个数！");
+            //遍历工作表每一行
+            for (int i = 4; i <= sheet.LastRowNum; i++)
+            {
+                try
+                {
+
+                    row = sheet.GetRow(i);   //row读入第i行数据  
+                    if (row != null)
+                    {
+                        if (row.GetCell(9) == null) {
+                            continue;
+                        }
+                        string cellValue = row.GetCell(9).ToString();
+                        if (string.IsNullOrEmpty(cellValue))
+                        {
+                            continue;
+                        }
+                        cellValue = cellValue.Trim();
+                        //把当前单元格的牌号拆分到Object[]里面
+                        IList<String[]> rowItems = getRowPhList(i + 1, cellValue);
+                        //获取牌号数量
+                        int phNum = int.Parse(row.GetCell(8).ToString().Trim());
+
+                        //显示不一样的牌号
+                        if (phNum != rowItems.Count)
+                        {
+                            int index = differGridView.Rows.Add();
+                            differGridView.Rows[index].Cells[0].Value = i + 1;
+                            differGridView.Rows[index].Cells[1].Value = phNum;
+                            differGridView.Rows[index].Cells[2].Value = rowItems.Count;
+                        }
+                        //合并每行数据
+                        MergeList(allItems, rowItems);
+
+                    }
+                }
+                catch
+                {
+                    throw new Exception("第" + (i + 1) +"行出现数据错误！");
+                }
+            }
+            return allItems;
+        }
+
+        /// <summary>
+        /// 将每个单元格的数字，通过逗号拆分后放入数组里面
+        /// [0]行数，[1]第几个，[2]值
+        /// </summary>
+        /// <param name="str">牌号单元格的值</param>
+        /// <returns></returns>
+        private IList<String[]> getRowPhList(int rowNum, String str)
+        {
+            //先将每行的中文逗号全部替换为英文的
+            str = str.Replace("，", ",");
+            //然后通过英文逗号拆分
+            String[] array = str.Split(',');
+            int itemNum = array.GetLength(0);
+            //判断最后一个字符是否为空，如果为空则元素个数减一
+            String lastItem = array[array.GetLength(0) - 1];
+            if (String.IsNullOrEmpty(lastItem))
+            {
+                itemNum = itemNum - 1;
+            }
+
+            //当前行的对象集合
+            IList<String[]> rowObjects = new List<String[]>();
+            //遍历每个编号，生成一个带位置的对象
+            for (int i = 0; i < itemNum; i++)
+            {
+                if (!String.IsNullOrEmpty(array[i]))
+                {
+                    getPh(rowObjects, array[i].Trim(), rowNum, i + 1);
+                }
+            }
+
+            return rowObjects;
+        }
+
+        private Object[] MergeObjects(Object[] First, Object[] Second)
+        {
+            Object[] result = new Object[First.Length + Second.Length];
+            First.CopyTo(result, 0);
+            Second.CopyTo(result, First.Length);
+            return result;
+        }
+
+        private void MergeList(IList<String[]> total, IList<String[]> rows)
+        {
+            foreach (String[] item in rows)
+            {
+                total.Add(item);
+            }
+        }
+
+        private void getPh(IList<String[]> rowObjects, String value, int rowNum, int columnNum)
+        {
+            //检查是否连续编号
+            if (value.IndexOf("~") == -1)
+            { //非连续编号
+                String[] item = new String[3];
+                item[0] = value.Trim();   //值
+                item[1] = rowNum.ToString(); //行数
+                item[2] = columnNum.ToString(); //第几个
+                rowObjects.Add(item);
+            }
+            else
+            { //连续编号
+                String[] phs = value.Split('~');
+                if (phs.Length != 2)
+                {
+                    throw new Exception("第" + rowNum + "行的连续编号'" + value + "'有误！");
+                }
+
+                if (phs[0].Length != phs[1].Length)
+                {
+                    throw new Exception("第" + rowNum + "行的连续编号'"
+                        + value + "'起止号长度不一致！"); ;
+                }
+
+                int index = 0; //记录起止号不一样的字符位置，用于生成连续号。
+                Char[] c_start = phs[0].ToCharArray();
+                Char[] c_end = phs[1].ToCharArray();
+                //找出index
+                for (int j = 0; j < c_start.Length; j++)
+                {
+                    if (c_start[j] != c_end[j])
+                    {
+                        index = j;
+                        break;
+                    }
+                }
+                int start = int.Parse(phs[0].Substring(index));
+                int end = int.Parse(phs[1].Substring(index));
+                String prefix = phs[0].Substring(0, index);
+
+                //从起始编号到结束编号
+                for (int i = start; i <= end; i++)
+                {
+                    String[] item = new String[3];
+                    item[0] = prefix + i.ToString();  //值
+                    item[1] = rowNum.ToString(); //行数
+                    item[2] = columnNum.ToString(); //第几个
+                    rowObjects.Add(item);
+                }
+            }
+        }
+
+        private void doQuery(List<String[]> list)
+        {
+            //遍历每一个牌号对象          
+            for (int i = 0; i < list.Count; i++)
+            {
+                String[] item = list[i];
+                //跳过为空的值
+                if (item[0] == "")
+                {
+                    continue;
+                }
+                String rowNum = ""; //记录行号，用于列表显示
+                //和剩余的牌号对象进行比较
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    String[] itemAfter = list[j];
+                    //是否重复
+                    if (item[0].Trim() == itemAfter[0].Trim())
+                    {
+                        rowNum += "," + itemAfter[1] + "行第" + itemAfter[2] + "个"; //行列
+                        itemAfter[0] = "";  //把重复的号置空，以后不比较。
+                    }
+                }
+                if (!string.IsNullOrEmpty(rowNum))
+                {
+                    String currentItem = item[1] + "行第" + item[2] + "个";
+                    int index = dataGridView2.Rows.Add();
+                    dataGridView2.Rows[index].Cells[0].Value = item[0];
+                    dataGridView2.Rows[index].Cells[1].Value = currentItem + rowNum;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            doAnalysisDate(tb_fileName.Text);
+        }
+    }
+}
